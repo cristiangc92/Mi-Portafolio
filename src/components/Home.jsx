6205af7939067387f7d359c43f548a8886a53536
unknown <cristiangc92@gmail.com>
2023-08-21 14:58:51 -0300

    Vista para tablet listo.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,15 @@ import Social from "./Social";
 const Home = () => {
   return (
     <div id="home">
-      <h2 className="tituloHome">
-        Hola! 👋🏼
-        <br />
-        <br /> Me llamo <span>Cristian</span> <br />
-        <br /> Full Stack <span>Web Developer</span>
-      </h2>
-      <Social />
+      <div>
+        <h2 className="tituloHome">
+          Hola! 👋🏼
+          <br />
+          <br /> Me llamo <span>Cristian</span> <br />
+          <br /> Full Stack <span>Web Developer</span>
+        </h2>
+        <Social />
+      </div>
       <img className="fotoPerfil" src={perfil} alt="" />
     </div>
   );
